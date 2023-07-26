72745708e94f4f65fea03821b53c819dc9cdaba7
lee14k <kmkovach@mtu.edu>
2023-07-26 10:26:45 -0700

    trying to layout homepage and navbar - added custom hook

---
+++ components/Homeslider.jsx
@@ -0,0 +1,46 @@
+'use client'
+import React, { useState, useEffect } from "react";
+import "./Homeslider.css";
+
+const images = [
+  './frc.jpeg', // replace with your image URLs
+  './frcneon.jpeg', 
+  './frcmugwall.jpeg',
+  // add more URLs as needed
+];
+
+function Homeslider() {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setActiveIndex((prevIndex) => 
+          prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        );
+      }, 3000); // Change slide every 60 seconds
+  
+      return () => clearInterval(interval);
+    }, [activeIndex]); // add activeIndex as dependency to re-run useEffect when activeIndex changes
+  
+    return (
+      <div className="slideshow">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`slide ${index === activeIndex ? "active" : ""}`}
+          style={{ backgroundImage: `url(${image})` }}
+        />
+      ))}
+      <div className="indicator">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            className={`indicator-dot ${index === activeIndex ? "active" : ""}`}
+          />
+        ))}
+      </div>
+    </div>
+    );
+  }
+
+export default Homeslider;
---
+++ components/Mid.jsx
@@ -0,0 +1,20 @@
+import './Mid.css'
+import Image from 'next/image'
+
+export default function Mid () {
+
+    return (
+        <div className='mid'>
+            <Image 
+                src='/espressoicon.png'
+                width={100}
+                height={100}
+                alt='espresso icon'
+            />
+            <h1 className='text-white'>Munising's Living Room</h1>
+            <p>From the local artist exhibits, to the live music and events that inspire us, to the thousands of hand-selected books among the shelves, the Falling Rock Café is much more than the comfy atmosphere, the delicious food, and the custom roasted coffee we serve.
+<button>Our Story</button>
+</p>
+        </div>
+    )
+}
